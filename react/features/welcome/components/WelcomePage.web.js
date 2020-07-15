dbd6218130a4537e947ff555a650aa785626f64a
root <root@dev.jitsi-meet.org>
2020-07-15 06:48:56 +0000

    start ver0.1

--- react/features/welcome/components/WelcomePage.web.js
+++ react/features/welcome/components/WelcomePage.web.js
@@ -186,7 +186,8 @@ class WelcomePage extends AbstractWelcomePage {
                     <div className = 'header-image' />
                     <div className = 'header-text'>
                         <h1 className = 'header-text-title'>
-                            { t('welcomepage.title') }
+                           // { t('welcomepage.title') }
+		           안전하고, 완벽한 기능을 갖춘 완전 무료 화상 회의
                         </h1>
                         <p className = 'header-text-description'>
                             { t('welcomepage.appDescription',
